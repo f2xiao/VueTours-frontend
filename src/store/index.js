f498008918dae4798e2e08f96d42eee1e296cb43
f2xiao <fxiao.mecheng@gmail.com>
2022-04-29 22:27:32 -0400

    Setup store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import product from'./modules/product';
+import cart from './modules/cart';
 
 export default createStore({
   state: {
@@ -10,5 +12,7 @@ export default createStore({
   actions: {
   },
   modules: {
+    product,
+    cart
   }
 })
